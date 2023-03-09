45db8f6ac96a9a16d5d3ae441072f64a0fac4736
AlexDark <alexdarkovich@mail.ru>
2023-03-09 18:16:05 +0300

    factory pattern implemented

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,30 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import com.mjc.stage2.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+    private final String JDBC_DRIVER = "org.h2.Driver";
+    private final String DB_URL = "jdbc:h2:~/test";
+    private final String USER = "sa";
+    private final String PASSWORD = "";
+
+
+    @Override
+    public Connection createConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
 }
 
